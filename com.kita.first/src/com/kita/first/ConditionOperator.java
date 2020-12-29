028f60d7039e715493d37d9c572712d03ff18c4d
gudtjr1213 <tkqjqn@naver.com>
2020-12-29 15:50:42 +0900

    20.12.29 JAVA If문

--- com.kita.first/src/com/kita/first/ConditionOperator.java
+++ com.kita.first/src/com/kita/first/ConditionOperator.java
@@ -5,12 +5,12 @@ public class ConditionOperator {
 		// 조건연산자(삼항연산자) A ? B : c;
 		int n1 = 3;
 		int n2 = 2;
-		
 		boolean isOdd = (n1 % 2 == 1) ? true : false;
 		System.out.println(isOdd);
 		
 		String result = (n1 > n2) ? "맞음" : "아님";
 		System.out.println(result);
 		
+	
 	}
 }
