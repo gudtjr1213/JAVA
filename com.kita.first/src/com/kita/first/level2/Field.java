94089a247947ec2857c5f10ba15cab3148a9564e
gudtjr1213 <tkqjqn@naver.com>
2021-01-14 15:48:05 +0900

    2021.01.14

---
+++ com.kita.first/src/com/kita/first/level2/Field.java
@@ -0,0 +1,20 @@
+package com.kita.first.level2;
+
+public class Field {
+	//필드
+	String company;
+	String name;
+	String color;
+	int memory;
+	//생성자
+	
+	//메소드
+	void sendMsg()
+	{
+		
+	}
+	void call()
+	{
+		
+	}
+}
